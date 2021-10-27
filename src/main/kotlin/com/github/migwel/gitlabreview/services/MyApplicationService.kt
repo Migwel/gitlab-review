2f97e7a401d660099e1baec7424102bf0d3e5313
GitHub Action <action@github.com>
2021-10-27 19:56:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/migwel/gitlabreview/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/migwel/gitlabreview/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.migwel.gitlabreview.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.migwel.gitlabreview.MyBundle
 
 class MyApplicationService {
 
